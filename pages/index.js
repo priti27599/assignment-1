d6e01a1c2739c46f0f5533b3130823236df9cd0f
priti27599 <75195316+priti27599@users.noreply.github.com>
2022-02-22 14:47:10 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -26,28 +26,36 @@ function Home({ data }) {
       <Head>
         <title>Assignment</title>
       </Head>
-      <div className="flex flex-wrap mt-10 mb-10 justify-center ">
+       <div className="flex flex-wrap mt-12 mb-15 justify-center ">
         {data.map((cur) => (
           <div>
-            <div className="py-5 px-5 border h-25 w-65 ml-8 mr-5 rounded-t-sm border-gray border-b-0 bg-gray-100">
-              <h3 className="text-lg font-semibold">{cur.name}</h3>
-
-              <div className="flex items-center ">
-                <MailIcon className="h-4 mr-1 font-thin text-gray-500" />
-                <h1 className="text-sm text-gray-500">{cur.email}</h1>
+            <div className="py-5 px-5  ml-8 mr-5 border rounded-t bg-cyan-100 border-gray border-b-0  ">
+              <div className="bg-gray-100 h-50 w-75">
+                <img
+                  src={`https://avatars.dicebear.com/v2/avataaars/${cur.username}.svg?options[mood][]=happy`}
+                  className="h-50 w-40 justify-center ml-auto mr-auto "
+                />
               </div>
+              <div className="border pl-5 pb-5 bg-white">
+                <h3 className="text-lg font-semibold mt-3">{cur.name}</h3>
 
-              <div className="flex items-center ">
-                <PhoneIcon className="h-4 mr-1 font-thin text-gray-500" />
-                <h1 className="text-sm text-gray-500">{cur.phone}</h1>
-              </div>
+                <div className="flex items-center ">
+                  <MailIcon className="h-4 mr-1 font-thin text-gray-500" />
+                  <h1 className="text-sm text-gray-500">{cur.email}</h1>
+                </div>
 
-              <div className="flex items-center ">
-                <GlobeAltIcon className="h-4 mr-1 font-thin text-gray-500" />
-                <h1 className="text-sm text-gray-500">{cur.website}</h1>
+                <div className="flex items-center ">
+                  <PhoneIcon className="h-4 mr-1 font-thin text-gray-500" />
+                  <h1 className="text-sm text-gray-500">{cur.phone}</h1>
+                </div>
+
+                <div className="flex items-center ">
+                  <GlobeAltIcon className="h-4 mr-1 font-thin text-gray-500" />
+                  <h1 className="text-sm text-gray-500">{cur.website}</h1>
+                </div>
               </div>
             </div>
-            <div className="flex mb-6 border-2 w-65 pt-2 pb-2 ml-8 mr-5 rounded-b-sm bg-sky-200">
+            <div className="flex mb-6 border-1 w-65 pt-2 pb-2 ml-8 mr-5 rounded-b bg-cyan-300">
               <HeartIcon className="h-6 mr-10 ml-10 text-red-500 hover:text-red-700  cursor-pointer" />
               <PencilAltIcon className="h-6 mr-20 ml-10  text-gray-500 cursor-pointer" />
               <TrashIcon className="h-6 mr-10 text-gray-500 cursor-pointer" />
